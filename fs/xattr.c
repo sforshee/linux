7b1b0a5824d95e69569eca8fb05778b3910e0608
Seth Forshee (DigitalOcean) <sforshee@kernel.org>
2022-09-21 08:10:09 -0500

    vfs: use strcmp instead of strncmp for checking xattr name
    Was missing an argument for strncmp(), but using strncmp() adds no value
    over strcmp() here. Squash this into commit introducing the problem.
    
    Signed-off-by: Seth Forshee (DigitalOcean) <sforshee@kernel.org>

--- fs/xattr.c
+++ fs/xattr.c
@@ -418,7 +418,7 @@ vfs_setxattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 	const void  *orig_value = value;
 	int error;
 
-	if (!strncmp(name, XATTR_NAME_CAPS))
+	if (!strcmp(name, XATTR_NAME_CAPS))
 		return -EOPNOTSUPP;
 
 	if (size && is_posix_acl_xattr(name))
@@ -535,7 +535,7 @@ vfs_getxattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 	struct inode *inode = dentry->d_inode;
 	int error;
 
-	if (!strncmp(name, XATTR_NAME_CAPS))
+	if (!strcmp(name, XATTR_NAME_CAPS))
 		return -EOPNOTSUPP;
 
 	error = xattr_permission(mnt_userns, inode, name, MAY_READ);
