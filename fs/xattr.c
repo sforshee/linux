931ee2ea6c238826a993c55e765e49eed40b73ea
Seth Forshee (DigitalOcean) <sforshee@kernel.org>
2023-10-11 07:50:06 -0500

    vfs: return -EOPNOTSUPP for fscaps from vfs_*xattr()
    Now that the new vfs-level interfaces are fully supported and all code
    has been converted to use them, stop permitting use of the top-level vfs
    xattr interfaces for capabilities xattrs. Unlike with ACLs we still need
    to be able to work with fscaps xattrs using lower-level interfaces in a
    handful of places, so only use of the top-level xattr interfaces is
    restricted.
    
    Signed-off-by: Seth Forshee (DigitalOcean) <sforshee@kernel.org>

--- fs/xattr.c
+++ fs/xattr.c
@@ -540,6 +540,9 @@ vfs_setxattr(struct mnt_idmap *idmap, struct dentry *dentry,
 	const void  *orig_value = value;
 	int error;
 
+	if (!strcmp(name, XATTR_NAME_CAPS))
+		return -EOPNOTSUPP;
+
 retry_deleg:
 	inode_lock(inode);
 	error = __vfs_setxattr_locked(idmap, dentry, name, value, size,
@@ -654,6 +657,9 @@ vfs_getxattr(struct mnt_idmap *idmap, struct dentry *dentry,
 {
 	struct inode *inode = dentry->d_inode;
 	int error;
+
+	if (!strcmp(name, XATTR_NAME_CAPS))
+		return -EOPNOTSUPP;
 
 	error = xattr_permission(idmap, inode, name, MAY_READ);
 	if (error)
@@ -793,6 +799,9 @@ vfs_removexattr(struct mnt_idmap *idmap, struct dentry *dentry,
 	struct inode *inode = dentry->d_inode;
 	struct inode *delegated_inode = NULL;
 	int error;
+
+	if (!strcmp(name, XATTR_NAME_CAPS))
+		return -EOPNOTSUPP;
 
 retry_deleg:
 	inode_lock(inode);
